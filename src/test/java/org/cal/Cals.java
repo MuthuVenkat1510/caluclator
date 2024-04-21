20568461a5ffc2d0837da757b1d559bc37244b02
MuthuVenkat1510 <v.mkumar15@gmail.com>
2024-04-21 08:52:56 +0530

    ref

---
+++ src/test/java/org/cal/Cals.java
@@ -0,0 +1,59 @@
+package org.cal;
+
+import java.util.*;
+
+public class Cals {
+	public static void main(String[] args) {
+//		for (int i = 1; i > 0; i++) {
+//
+//			Scanner sc = new Scanner(System.in);
+//			System.err.println("Welcome to My Calculator");
+//			System.err.println("========================");
+//			System.out.println("Press 1 for Addition");
+//			System.out.println("Press 2 for Subraction");
+//			System.out.println("press 3 for mulitiplication");
+//			System.out.println("Press 4 for Division");
+//			System.out.println("press 0 for Exit ");
+//			int option = sc.nextInt();
+//			switch (option) {
+//			case 1:
+//				System.out.println("Enter the value");
+//				int val1 = sc.nextInt();
+//				System.out.println("Enter the Value");
+//				int val2 = sc.nextInt();
+//				System.out.println("Addition of Given value " + "  "+val1 +" + "+ val2 +" = "+ (val1 + val2));
+//				break;
+//			case 2:
+//				System.out.println("Enter the value");
+//				int val3 = sc.nextInt();
+//				System.out.println("Enter the Value");
+//				int val4 = sc.nextInt();
+//				System.out.println("Subraction of Given value " + "  "+val3 +" - "+ val4 +" = "+ (val3 - val4));
+//				break;
+//			case 3:
+//				System.out.println("Enter the value");
+//				int val5 = sc.nextInt();
+//				System.out.println("Enter the Value");
+//				int val6 = sc.nextInt();
+//				System.out.println("Mulitiplication of Given value " +"  "+ val5 +" * "+ val6 +" = "+ (val5 * val6));
+//				break;
+//			case 4:
+//				System.out.println("Enter the value");
+//				int val7 = sc.nextInt();
+//				System.out.println("Enter the Value");
+//				int val8 = sc.nextInt();
+//				System.out.println("Division of Given value " + "  "+val7 +" / "+ val8 +" = "+ (val7 / val8));
+//				break;
+//			case 0:
+//				System.err.println("<........Thank You For Using.........>");
+//				System.err.println("======================================");
+//				System.exit(0);
+//				break;
+//			default:
+//				System.out.println("Invalid Entry ");
+//				break;
+//			}
+//		}
+		System.out.println("hdh");
+	}
+}
